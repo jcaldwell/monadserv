a8fcc87f7d2cd06cc26d1228b497354f87be8653
Jeff Caldwell <jeffery.caldwell@gmail.com>
2008-12-24 13:22:13 -0500

    modified to incorporate concurrency ideas from turbinado.

--- src/MonadServ/DemoLoop.hs
+++ src/MonadServ/DemoLoop.hs
@@ -11,7 +11,7 @@ import Control.Concurrent
 import System.IO
 import System.Locale
 import System.Time
-
+import Data.Time
 import Data.Char
 import qualified Data.Map as DataMap
 import Data.List
@@ -21,30 +21,56 @@ import qualified Control.Exception as Ex
 data InternalServerState 
    = InternalServerState
      { isLogEnabled    :: Bool
-     , sock            :: Maybe Socket  }
+     , sock            :: Maybe Socket
+     , pool            :: MVar WorkerPool  }
+
+type Environment = [String]
 
 defaultInternalServerState = InternalServerState { isLogEnabled = False, sock = Nothing }
 
 main = do
     (port:_) <- getArgs
     sck <- listenOn (PortNumber (fromIntegral (read port :: Int)))
-    let iss =  defaultInternalServerState { sock = Just sck }
+    workerPoolMVar <- newMVar $ WorkerPool 0 [] []
+    let iss =  defaultInternalServerState { sock = Just sck, pool = workerPoolMVar }
     putStrLn $ "server is starting on port: " ++ show port
     runServer iss
 
 
 runServer :: InternalServerState -> IO ()
-runServer iss@(InternalServerState isLogEnabled (Just socket)) =  Ex.bracket setup exit loop
+runServer iss@(InternalServerState isLogEnabled (Just socket) workerPoolMVar) =  Ex.bracket setup exit loop
     where  setup = do putStrLn "ready for requests..."
-                      return socket
-           loop s  = do accept s >>= forkIO . work
-                        loop s
-           exit s = do putStrLn "Exiting..."
-                       sClose s
+                      return (socket, workerPoolMVar)
+
+           loop (s, workerPoolMVar)  = do (sock', sockAddr) <- accept s
+                                          WorkerThread _ chan <- getWorkerThread workerPoolMVar []
+                                          writeChan chan sock'
+                                          loop (s, workerPoolMVar)
+
+           exit (s, _) = do putStrLn "Exiting..."
+                            sClose s
 
 
-work :: (Socket, SockAddr) -> IO ()
-work (sock', sockAddr)  = do
+------------------------------------------------
+-- | Worker stuff
+------------------------------------------------
+
+workerLoop :: MVar WorkerPool ->
+              Environment     ->
+              Chan Socket     ->
+              IO ()
+workerLoop workerPoolMVar e chan
+    = do mainLoop
+    where
+      mainLoop
+          = do sock      <- readChan chan
+               work sock e
+               putWorkerThread workerPoolMVar chan
+               mainLoop
+
+
+work :: Socket -> Environment -> IO ()
+work sock' _  = do
     r <- getRequest sock'
     case r of
           Just req@(Request uri method headers body) -> do putStrLn "request received"
@@ -52,11 +78,63 @@ work (sock', sockAddr)  = do
           Nothing -> putStrLn "error caught..."
     sClose sock'
 
-
-
 buildResponse :: URI -> String
 buildResponse uri@(URI scheme _ path query fragment) = "You have requested-- scheme[" ++ scheme ++ "] path [" ++ path ++ "] query [" ++ query ++ "] fragment ["++ fragment ++ "]"
 
+
+
+
+------------------------------------------------
+-- | Worker Pool stuff
+------------------------------------------------
+
+type ExpiresTime = UTCTime
+data WorkerThread = WorkerThread ThreadId (Chan Socket)
+data WorkerPool = WorkerPool { numWorkers :: Int,
+                               idleWorkers :: [WorkerThread],
+                               busyWorkers :: [(WorkerThread, ExpiresTime)]}
+
+--getWorkerThread :: MVar WorkerPool -> IO WorkerThread
+getWorkerThread mv e = 
+  do wp <- takeMVar mv
+     case wp of
+       WorkerPool n [] bs -> 
+         do chan <- newChan
+--            e' <- runController (addDatabaseToEnvironment) e
+--            tid <- forkIO $ workerLoop mv e' chan
+            tid <- forkIO $ workerLoop mv e chan
+            let workerThread = WorkerThread tid chan 
+            expiresTime <- getCurrentTime >>= \utct -> return $ addUTCTime (fromInteger stdTimeOut) utct
+            putMVar mv $ WorkerPool (n+1) [] ((workerThread, expiresTime):bs)
+            return workerThread
+       WorkerPool n (idle:idles) busies ->
+         do expiresTime <- getCurrentTime >>= \utct -> return $ addUTCTime (fromInteger stdTimeOut) utct
+            putMVar mv $ WorkerPool n idles ((idle, expiresTime):busies)
+            return idle
+
+putWorkerThread mv chan = do
+               WorkerPool n is bs <- takeMVar mv
+               mytid <- myThreadId
+               let bs' = filter (\(WorkerThread tid _, _) -> tid /= mytid) bs
+               putMVar mv $ WorkerPool n ((WorkerThread mytid chan):is) bs'
+
+
+{--
+timeout :: Int -> ThreadId -> IO ()
+timeout time thid
+    = do threadDelay time
+         throwTurbinadoTo thid TimedOut
+--}
+
+-- conf. files? Indeed! haha
+stdTimeOut :: Integer
+stdTimeOut = 90
+
+
+
+------------------------------------------------
+-- | HTTP Stuff
+------------------------------------------------
 
 getRequest :: Socket  -> IO (Maybe Request)
 getRequest sock = do
@@ -67,7 +145,7 @@ getRequest sock = do
 
 sendResponse :: Socket -> String -> IO ()
 sendResponse sock xs = do t <- getClockTime
-                          let resp = Response (2,0,0) "OK" (buildHeaders (Just $ length xs) t (startingHeaders t)) (xs)
+                          let resp = Response (2,0,0) "OK" (buildHeaders (Just $ length xs) t []) (xs)
                           respondHTTP sock resp
 
 
